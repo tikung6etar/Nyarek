13a298447e52b7434c2d541dd6401bcf2a03d359
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2025-11-29 22:28:36 +0700

    Update foxd.php

--- foxd.php
+++ foxd.php
@@ -19,8 +19,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Bungee+Outline&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 	@import url('https://fonts.googleapis.com/css?family=Dosis');
 	@import url('https://fonts.googleapis.com/css?family=Bungee');
